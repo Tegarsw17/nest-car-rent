4728808c25fe4591fe1f521e508da3426cf3fabf
Tegarsw17 <tegarsatriyawiguna99@gmail.com>
2025-04-09 21:27:51 +0700

    update configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { CarModule } from './car/car.module';
+import { AdminCarModule } from './admin-car/admin-car.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL, // or paste the URI directly here
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UserModule,
+    AuthModule,
+    CarModule,
+    AdminCarModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: 'http://localhost:5173', // or your Vite app URL
+    credentials: true, // if you’re using cookies/auth
+  });
+
+  await app.listen(3100);
 }
 bootstrap();
